266b289b794ac8e6ba6ea997ec4babef7317fddc
Vlad <vlad.kosevichhhh@gmail.com>
2023-06-26 10:36:37 +0300

    seed error

--- seeder/seed.ts
+++ seeder/seed.ts
@@ -1,36 +1,36 @@
+import { PrismaClient } from '@prisma/client'
 import { faker } from '@faker-js/faker'
-import { PrismaClient, Product } from '@prisma/client'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 const prisma = new PrismaClient()
 
 const createProducts = async (quantity: number) => {
-	const products: Product[] = []
+	const products = []
 
 	for (let i = 0; i < quantity; i++) {
 		const productName = faker.commerce.productName()
-		const categoryName = faker.commerce.productName()
+		const categoryName = faker.commerce.department()
 
 		const product = await prisma.product.create({
 			data: {
 				name: productName,
 				slug: faker.helpers.slugify(productName),
 				description: faker.commerce.productDescription(),
-				price: +faker.commerce.price(10, 999, 0),
+				price: +faker.commerce.price({ min: 10, max: 999 }),
 				images: Array.from({
-					length: faker.datatype.number({ min: 2, max: 6 })
-				}).map(() => faker.image.imageUrl()),
+					length: faker.number.int({ min: 2, max: 6 })
+				}).map(() => faker.image.url()),
 				category: {
 					create: {
 						name: categoryName,
-						slug: faker.helpers.slugify(categoryName)
+						slug: categoryName
 					}
 				},
 				reviews: {
 					create: [
 						{
-							rating: faker.datatype.number({ min: 1, max: 5 }),
+							rating: faker.number.int({ min: 1, max: 5 }),
 							text: faker.lorem.paragraph(),
 							user: {
 								connect: {
@@ -39,7 +39,7 @@ const createProducts = async (quantity: number) => {
 							}
 						},
 						{
-							rating: faker.datatype.number({ min: 1, max: 5 }),
+							rating: faker.number.int({ min: 1, max: 5 }),
 							text: faker.lorem.paragraph(),
 							user: {
 								connect: {
@@ -51,13 +51,15 @@ const createProducts = async (quantity: number) => {
 				}
 			}
 		})
+
 		products.push(product)
 	}
-	console.log(`Created ${products.length} products`)
+
+	console.log(`Создано ${products.length} продуктов`)
 }
 
 async function main() {
-	console.log('Start seeding ...')
+	console.log('Начало заполнения базы данных ...')
 	await createProducts(10)
 }
 
