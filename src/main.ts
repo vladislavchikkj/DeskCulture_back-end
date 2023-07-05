91d2309d1e62b61b31fe065b71558189d352111a
Vlad <vlad.kosevich@mail.ru>
2023-07-05 21:29:08 +0300

    fix add to fav

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma.service'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	//
+	const config = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.addTag('cats')
+		.addBearerAuth()
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('test', app, document)
 
 	const prismaService = app.get(PrismaService)
 	await prismaService.enableShutdownHooks(app)
-
+	//
 	app.setGlobalPrefix('api')
 	app.enableCors()
 	await app.listen(4200)
